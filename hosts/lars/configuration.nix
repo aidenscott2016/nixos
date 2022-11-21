0fdc17e7008f75ec6ec1697f23eb6681ce5b36d3
Aiden Scott <aiden@oldstreetjournal.co.uk>
2022-11-21 20:33:10 +0000

    set up firewall rules for xdebug over docker

--- hosts/lars/configuration.nix
+++ hosts/lars/configuration.nix
@@ -7,6 +7,14 @@
 {
   imports = [ ./hardware-configuration.nix ../../common/packages.nix ];
   nix.settings.auto-optimise-store = true;
+  networking.firewall = {
+    logRefusedConnections = true;
+    enable = true;
+
+    # xdebug. I want to narrow this down to  just the docker interface but the veth changes every time
+    allowedTCPPorts = [ 9000 ];
+    allowedUDPPorts = [ 9000 ];
+  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
