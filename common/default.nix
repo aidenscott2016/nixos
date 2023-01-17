b4b6d2ca3f19d6b78a0d4cdcd0a3bbc2996e33f1
Aiden <aiden@oldstreetjournal.co.uk>
2023-01-17 00:10:37 +0000

    extract common

--- common/default.nix
+++ common/default.nix
@@ -1,23 +1,33 @@
 { config, pkgs, lib, ... }:
 
+with lib;
 {
-  system.stateVersion = "22.05";
-  nixpkgs.config.allowUnfree = true;
-  nix.extraOptions = "experimental-features = nix-command flakes";
-  nix.settings.auto-optimise-store = true;
-
-  users.groups.cheese = { };
-  users.users.aiden = {
-    initialPassword = "password";
-    isNormalUser = true;
-    extraGroups = [ "wheel" "disk" "docker" "cheese" ];
+  options = {
+    extraGroups = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      description = lib.mdDoc "The user's auxiliary groups.";
+    };
   };
+  config = {
+    system.stateVersion = "22.05";
+    nixpkgs.config.allowUnfree = true;
+    nix.extraOptions = "experimental-features = nix-command flakes";
+    nix.settings.auto-optimise-store = true;
 
-  time.timeZone = "Europe/London";
-  i18n.defaultLocale = "en_GB.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    useXkbConfig = true;
+    users.groups.cheese = { };
+    users.users.aiden = {
+      initialPassword = "password";
+      isNormalUser = true;
+      extraGroups = [ "wheel" "disk" "docker" "cheese" ];
+    };
+
+    time.timeZone = "Europe/London";
+    i18n.defaultLocale = "en_GB.UTF-8";
+    console = {
+      font = "Lat2-Terminus16";
+      useXkbConfig = true;
+    };
   };
 
 }
