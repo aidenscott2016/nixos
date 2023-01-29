34a0bb943e22385377974880af5c59e222c953e3
Aiden <aiden@oldstreetjournal.co.uk>
2023-01-29 09:56:27 +0000

    actually commit the autorandr script

--- common/default.nix
+++ common/default.nix
@@ -12,7 +12,7 @@ with lib;
     users.users.aiden = {
       initialPassword = "password";
       isNormalUser = true;
-      extraGroups = [ "wheel" "disk" "docker" "cheese" "networkmanager" ];
+      extraGroups = [ "wheel" "disk" "docker" "cheese" "networkmanager" "video" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgHxgT0rlJDXl+opb7o2JSfjd5lJZ6QTRr57N0MIAyN aiden@lars"
       ];
--- hosts/locutus/autorandr/default.nix
+++ hosts/locutus/autorandr/default.nix
@@ -1,4 +1,8 @@
 { lib, pkgs, config, ... }:
+with lib;
+let
+  cfg = config.services.autorandr;
+in
 {
   services.autorandr = {
     enable = true;
@@ -7,5 +11,27 @@
   # I am using config files rather than the module's options becuase
   # they do not support all the attrbites required for autorandr to
   # detect the profile as 'current' I think it's some `x-*` prop
+
+  # systemd.user.services.autorandr-logon = {
+  #   wantedBy = [ "graphical-session.target" ];
+  #   after = [ "graphical-session.target" ];
+  #   enable = true;
+  #   description = "Autorandr logon execution hook";
+  #   serviceConfig.PassEnvironment = "DISPLAY";
+
+  #   serviceConfig = {
+  #     ExecStart = ''
+  #       ${pkgs.autorandr}/bin/autorandr docked
+  #     '';
+  #     Type = "oneshot";
+  #     # RemainAfterExit = false;
+  #     # KillMode = "process";
+  #   };
+  # };
   environment.etc."xdg/autorandr".source = ./profiles;
 }
+
+
+
+
+# ${# optionalString cfg.ignoreLid "--ignore-lid"}
