93eea284aca090133cad92388aea7aec8a2818af
Aiden <aiden@oldstreetjournal.co.uk>
2023-01-29 05:03:10 +0000

    extract docker-php, ssh

--- hosts/locutus/configuration.nix
+++ hosts/locutus/configuration.nix
@@ -9,22 +9,23 @@
     ../../modules/redshift.nix
     ../../modules/printer.nix
     ../../modules/ssh.nix
+    ../../modules/php-docker.nix
     ./disko.nix
   ];
 
-  networking.firewall = {
-    logRefusedConnections = true;
-    enable = true;
-    # xdebug. I want to narrow this down to just the docker interface but the veth changes every time
-    allowedTCPPorts = [ 9000 ];
-    allowedUDPPorts = [ 9000 ];
-  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+    };
+  };
 
   networking.hostName = "locutus";
   networking.networkmanager.enable = true;
+  programs.nm-applet.enable = true;
 
 
 
@@ -48,15 +49,12 @@
 
   security.sudo.wheelNeedsPassword = false;
 
-  virtualisation.docker.enable = true;
 
-  programs.gnupg.agent = {
-    enable = true;
-    pinentryFlavor = "gtk2";
-    enableSSHSupport = true;
-  };
-
-  programs.nm-applet.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   pinentryFlavor = "gtk2";
+  #   enableSSHSupport = true;
+  # };
 
   programs.steam = {
     enable = true;
@@ -65,18 +63,12 @@
   };
 
 
-  boot.initrd.luks.devices = {
-    root = {
-      device = "/dev/nvme0n1p2";
-      preLVM = true;
-    };
-  };
 
 
   services.gvfs.enable = true;
 
 
-
+  # move this to dwm some time
   services.picom = {
     enable = true;
     vSync = true;
