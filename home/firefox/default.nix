36dc4f3347782eb235c5c83b4ba140a01cf9ebc2
Aiden <aiden@oldstreetjournal.co.uk>
2023-01-29 07:22:57 +0000

    disable pocket in ff

--- home/firefox/default.nix
+++ home/firefox/default.nix
@@ -14,12 +14,14 @@ in
         "browser.download.autohideButton" = false;
         "browser.uiCustomization.state" = ''
 
-          {"placements":{"widget-overflow-fixed-list":["save-to-pocket-button"],"unified-extensions-area":[],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","downloads-button","fxa-toolbar-menu-button","sponsorblocker_ajay_app-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","sponsorblocker_ajay_app-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":3}
-        '';
+{"placements":{"widget-overflow-fixed-list":["save-to-pocket-button"],"unified-extensions-area":[],"nav-bar":["back-button","forward-button","stop-reload-button","urlbar-container","downloads-button","fxa-toolbar-menu-button","sponsorblocker_ajay_app-browser-action","ublock0_raymondhill_net-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","sponsorblocker_ajay_app-browser-action","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":3}
+
+'';
         "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
         "browser.newtabpage.activity-stream.feeds.topsites" = false;
         "browser.newtabpage.activity-stream.showSearch" = false;
         "browser.urlbar.suggest.topsites" = false;
+        "extensions.pocket.enabled" = false;
       };
     };
     extensions = with addons; [
@@ -30,3 +32,4 @@ in
 
   };
 }
+
