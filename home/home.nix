f21f18d5d0af67faa69ea30bad6da9620a931b03
Aiden <aiden@oldstreetjournal.co.uk>
2023-09-26 13:43:33 +0100

    extract desktop specific parts of home-manager configuration

--- home/home.nix
+++ home/home.nix
@@ -1,20 +1,4 @@
 inputs@{ config, pkgs, lib, ... }: {
-  imports = [ ./git.nix ./tmux ./bash ./firefox ./gpg-agent.nix ./ledger.nix ];
-
-  home.stateVersion = "23.05";
-
-  xdg.enable = true;
-  xdg.configFile."discord/settings.json".text = ''{"SKIP_HOST_UPDATE": true}'';
-  xdg.configFile."emacs/init.el".source = ./files/init.el;
-
-  programs.ssh.enable = true;
-  programs.ssh.matchBlocks."gitlab.com".identityFile = "~/.ssh/gitlab";
-  programs.ssh.matchBlocks."github.com".identityFile = "~/.ssh/github";
-  programs.ssh.matchBlocks."10.0.4.*".identityFile = "~/.ssh/local";
-
-  home.file."downloads".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/aiden/Downloads";
-  home.file.".vimrc".source = ./files/vimrc;
-  home.file.".ideavimrc".source = ./files/ideavimrc;
+  imports = [ ./desktop.nix ];
 
 }
