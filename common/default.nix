fcd6cd6477c869aaa237808b559569447aed34e2
Aiden <aiden@oldstreetjournal.co.uk>
2023-09-26 18:19:09 +0100

    add trusted-users nix setting for deploys

--- common/default.nix
+++ common/default.nix
@@ -1,18 +1,19 @@
 { config, pkgs, lib, ... }:
 
-with lib;
-{
+with lib; {
   config = {
     system.stateVersion = "22.05";
     nixpkgs.config.allowUnfree = true;
     nix.extraOptions = "experimental-features = nix-command flakes";
     nix.settings.auto-optimise-store = true;
+    nix.settings.trusted-users = [ "aiden" ];
 
     users.groups.cheese = { };
     users.users.aiden = {
       initialPassword = "password";
       isNormalUser = true;
-      extraGroups = [ "wheel" "disk" "docker" "cheese" "networkmanager" "video" ];
+      extraGroups =
+        [ "wheel" "disk" "docker" "cheese" "networkmanager" "video" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgHxgT0rlJDXl+opb7o2JSfjd5lJZ6QTRr57N0MIAyN aiden@lars"
       ];
@@ -25,17 +26,15 @@ with lib;
       font = "Lat2-Terminus16";
       useXkbConfig = true;
     };
-    services.xserver =
-      {
-        layout = "gb";
-        xkbOptions = "caps:swapescape";
-        libinput.enable = true;
-      };
+    services.xserver = {
+      layout = "gb";
+      xkbOptions = "caps:swapescape";
+      libinput.enable = true;
+    };
 
-    environment.sessionVariables =
-      {
-        EDITOR = "vim";
-        VISUAL = "vim";
-      };
+    environment.sessionVariables = {
+      EDITOR = "vim";
+      VISUAL = "vim";
+    };
   };
 }
