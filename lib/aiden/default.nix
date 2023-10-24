a7e5361947a943c24ae12524b78a18ec8796a263
Aiden <aiden@oldstreetjournal.co.uk>
2023-10-24 18:14:31 +0100

    Add my first lib function

---
+++ lib/aiden/default.nix
@@ -0,0 +1,16 @@
+{ lib, ... }:
+with lib.aiden;
+with lib; {
+  enableableModule = name:
+    params@{ config, ... }:
+    configToEnable:
+    let cfg = config.aiden.modules.${name};
+    in {
+      options.aiden.modules.${name}.enabled = mkOption {
+        type = types.bool;
+        default = false;
+      };
+
+      config = mkIf cfg.enabled configToEnable;
+    };
+}
