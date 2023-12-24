11b2fe5c4a4461646994ced2c0b7ae764950e49e
Aiden <aiden@oldstreetjournal.co.uk>
2023-12-24 13:11:21 +0000

    Adds `devices` param to HA module
    A list of devices to pass through to the HA OCI image

--- modules/nixos/home-assistant/default.nix
+++ modules/nixos/home-assistant/default.nix
@@ -1,14 +1,26 @@
 params@{ pkgs, lib, config, ... }:
-with lib.aiden;
-enableableModule "home-assistant" params {
-  virtualisation.oci-containers = {
+with lib;
+let
+  deviceParams =
+    map (path: "--device=${path}") config.aiden.modules.home-assistant.devices;
+in {
+  options.aiden.modules.home-assistant = {
+    enabled = mkEnableOption "home-assistant";
+    devices = mkOption {
+      type = with types; listOf str;
+      example = ["/dev/serial/by-id/usb-Nabu_Casa_SkyConnect_v1.0_2ee577279f96ed119403c098a7669f5d-if00-port0"];
+      description = "A list of devices to mount in the HA image";
+      default = [ ];
+    };
+  };
+  config.virtualisation.oci-containers = {
     backend = "podman";
     containers.homeassistant = {
       volumes = [ "home-assistant:/config" ];
       environment.TZ = "Europe/London";
       image =
-        "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
-      extraOptions = [ "--network=host" ];
+        "ghcr.io/home-assistant/home-assistant:stable"; 
+      extraOptions = [ "--network=host" ] ++ deviceParams;
     };
   };
 }
