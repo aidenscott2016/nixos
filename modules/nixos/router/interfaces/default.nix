8fada520e7cd802702ff5fe981bd1687a9512e69
Aiden <aiden@oldstreetjournal.co.uk>
2023-12-25 16:03:50 +0000

    modify ip addresses for vlan

--- modules/nixos/router/interfaces/default.nix
+++ modules/nixos/router/interfaces/default.nix
@@ -25,26 +25,26 @@ in {
         ${internalInterface} = { useDHCP = false; };
         lan = {
           ipv4.addresses = [{
-            address = "10.0.0.1";
+            address = "10.0.1.1";
             prefixLength = 24;
           }];
         };
         iot = {
           ipv4.addresses = [{
-            address = "10.0.1.1";
+            address = "10.0.2.1";
             prefixLength = 24;
           }];
         };
         guest = {
           ipv4.addresses = [{
-            address = "10.0.2.1";
+            address = "10.0.3.1";
             prefixLength = 24;
           }];
         };
         eth3 = {
           useDHCP = false;
           ipv4.addresses = [{
-            address = "10.0.3.1";
+            address = "10.0.4.1";
             prefixLength = 24;
           }];
         };
