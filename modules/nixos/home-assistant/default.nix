a536fefeb9d04657c1a885a2085426d94b0abb63
Aiden <aiden@oldstreetjournal.co.uk>
2023-12-26 12:11:49 +0000

    Add enabled flag to homeassistant

--- modules/nixos/home-assistant/default.nix
+++ modules/nixos/home-assistant/default.nix
@@ -3,23 +3,25 @@ with lib;
 let
   deviceParams =
     map (path: "--device=${path}") config.aiden.modules.home-assistant.devices;
-in {
+  enabled = config.aiden.modules.home-assistant.enabled;
+in
+{
   options.aiden.modules.home-assistant = {
     enabled = mkEnableOption "home-assistant";
     devices = mkOption {
       type = with types; listOf str;
-      example = ["/dev/serial/by-id/usb-Nabu_Casa_SkyConnect_v1.0_2ee577279f96ed119403c098a7669f5d-if00-port0"];
+      example = [ "/dev/serial/by-id/usb-Nabu_Casa_SkyConnect_v1.0_2ee577279f96ed119403c098a7669f5d-if00-port0" ];
       description = "A list of devices to mount in the HA image";
       default = [ ];
     };
   };
-  config.virtualisation.oci-containers = {
+  config.virtualisation.oci-containers = mkIf enabled {
     backend = "podman";
     containers.homeassistant = {
       volumes = [ "home-assistant:/config" ];
       environment.TZ = "Europe/London";
       image =
-        "ghcr.io/home-assistant/home-assistant:stable"; 
+        "ghcr.io/home-assistant/home-assistant:stable";
       extraOptions = [ "--network=host" ] ++ deviceParams;
     };
   };
