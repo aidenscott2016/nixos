c4abb1ae113f152566ee8c5feb561de40046c13f
Aiden <aiden@oldstreetjournal.co.uk>
2024-01-20 14:49:05 +0000

    config traefik to use existing certs

--- systems/x86_64-linux/gila/default.nix
+++ systems/x86_64-linux/gila/default.nix
@@ -53,16 +53,43 @@
   services.nginx.enable = lib.mkForce false;
 
 
-
-  users.users.traefik.extraGroups = [ "acme" ];
+  users.users.traefik.extraGroups = [ "acme" ]; # to read acme folder
   services.traefik = {
     enable = true;
     group = "podman";
     staticConfigOptions = {
-      providers.docker.endpoint = "unix:///var/run/podman/podman.sock";
+      accessLog = { };
+      log.level = "DEBUG";
+      global = {
+        checkNewVersion = false;
+        sendAnonymousUsage = false;
+      };
+      providers.docker = {
+        exposedByDefault = false;
+        endpoint = "unix:///var/run/podman/podman.sock";
+      };
       api.dashboard = true;
       api.insecure = true;
+      entrypoints = {
+        web = {
+          address = ":80";
+          http.redirections.entrypoint = {
+            to = "websecure";
+            scheme = "https";
+          };
+        };
+        websecure.address = ":443";
+      };
+    };
+    dynamicConfigOptions = {
+      tls = {
+        stores.default = {
+          defaultCertificate = {
+            certFile = "/var/lib/acme/sw1a1aa.uk/fullchain.pem";
+            keyFile = "/var/lib/acme/sw1a1aa.uk/key.pem";
+          };
+        };
+      };
     };
   };
-
 }
