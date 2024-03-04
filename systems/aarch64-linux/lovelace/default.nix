9d8e0d674de2beaed51e97be8dc3dea1162b4fbe
Aiden <aiden@oldstreetjournal.co.uk>
2024-03-04 14:07:15 +0000

    Extract TS to module

--- systems/aarch64-linux/lovelace/default.nix
+++ systems/aarch64-linux/lovelace/default.nix
@@ -28,6 +28,7 @@ with inputs; {
 
   aiden = {
     modules = {
+      tailscale = {enabled = true; authKeyPath = age.secrets.secret1.path}
       avahi.enabled = true;
       common.enabled = true;
     };
@@ -35,7 +36,6 @@ with inputs; {
 
 
   services = {
-    tailscale = { enable = true; openFirewall = true; };
     adguardhome = {
       enable = true;
       openFirewall = false;
@@ -43,35 +43,6 @@ with inputs; {
     };
   };
 
-  # create a oneshot job to authenticate to Tailscale
-  systemd.services.tailscale-autoconnect =
-    let authkeyPath = config.age.secrets.secret1.path;
-    in {
-      description = "Automatic connection to Tailscale";
-
-      # make sure tailscale is running before trying to connect to tailscale
-      after = [ "network-pre.target" "tailscale.service" ];
-      wants = [ "network-pre.target" "tailscale.service" ];
-      wantedBy = [ "multi-user.target" ];
-
-      # set this service as a oneshot job
-      serviceConfig.Type = "oneshot";
-
-      # have the job run this shell script
-      script = with pkgs; ''
-        # wait for tailscaled to settle
-        sleep 2
-        # check if we are already authenticated to tailscale
-        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-          exit 0
-        fi
-
-        # otherwise authenticate with tailscale
-        ${tailscale}/bin/tailscale up -authkey  file:${authkeyPath}
-
-      '';
-    };
   services.gvfs.enable = true;
   services.udisks2.enable = true;
 
