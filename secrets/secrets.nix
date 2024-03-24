8e7c11dbff57ebe7d789264dbdf6e06b2f245ca4
Aiden <aiden@oldstreetjournal.co.uk>
2024-03-24 15:36:46 +0000

    TH: add thoth

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,6 +6,7 @@ let
   lovelace =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHN4Z6+j8AU0Qiywv8sTjlG0UlY+ZAUzLWMSnqeY5U0f";
   gila = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9nT33O30eofxYEcnJRVunFvFmOB9VqPDWC9EC77+Lz";
+  thoth = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJuers0/aJpKmaxeD0CPHbQ6O422fkVO4i3JTLTtbW4I";
   systems = [ lovelace gila ];
 in
 {
@@ -13,5 +14,6 @@ in
   "lego-credentials.age".publicKeys = [ aiden ];
   "mosquitto-pass.age".publicKeys = [ aiden gila ];
   "gila-tailscale-authkey".publicKeys = [ gila ];
+  "thoth-tailscale-authkey".publicKeys = [ thoth ];
   "cf-token.age".publicKeys = [ aiden gila ];
 }
---
+++ systems/x86_64-linux/thoth/default.nix
@@ -0,0 +1,60 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ config, pkgs, inputs, modulesPath, ... }:
+
+
+with inputs; {
+  imports = [
+    ./hardware-configuration.nix
+    disko.nixosModules.disko
+    ./disk-config.nix
+    agenix.nixosModules.default
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  age.secrets.thoth-tailscale-authkey.file = "${self.outPath}/secrets/thoth-tailscale-authkey";
+  nixpkgs.hostPlatform = "aarch64-linux";
+  services.openssh.enable = true;
+  services.openssh.openFirewall = true;
+  security.sudo.wheelNeedsPassword = false;
+  networking.firewall = {
+    enable = true;
+    trustedInterfaces = [ "tailscale0" ];
+    allowedUDPPorts = [ 53 ];
+    allowedTCPPorts = [
+      53
+      config.services.adguardhome.settings.bind_port
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [ dnsutils tailscale ];
+
+
+  aiden = {
+    modules = {
+      tailscale = {
+        enabled = true;
+        authKeyPath = config.age.secrets.thoth-tailscale-authkey.path;
+      };
+      avahi.enabled = true;
+      common.enabled = true;
+    };
+  };
+
+  services = {
+    adguardhome = {
+      enable = true;
+      openFirewall = false;
+      settings.bind_host = "0.0.0.0";
+      settings.bind_port = 8081;
+      querylog.enabled = false;
+    };
+  };
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
+
