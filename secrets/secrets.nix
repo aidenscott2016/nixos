a2bc44a1d1e6789545e2b02bd828e07f226e0cfa
Aiden <aiden@oldstreetjournal.co.uk>
2024-03-27 06:26:24 +0000

    LC: rekey cf-token with LC key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,12 +2,11 @@ let
   aiden =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgHxgT0rlJDXl+opb7o2JSfjd5lJZ6QTRr57N0MIAyN";
   users = [ aiden ];
-
+  locutus =  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9g9o59uCL408pLv7RQglyvnoAHSwuitICOTVBu70X5";
   lovelace =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHN4Z6+j8AU0Qiywv8sTjlG0UlY+ZAUzLWMSnqeY5U0f";
   gila = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9nT33O30eofxYEcnJRVunFvFmOB9VqPDWC9EC77+Lz";
   thoth = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJuers0/aJpKmaxeD0CPHbQ6O422fkVO4i3JTLTtbW4I";
-  systems = [ lovelace gila ];
 in
 {
   "secret1.age".publicKeys = [ aiden lovelace ];
@@ -15,5 +14,5 @@ in
   "mosquitto-pass.age".publicKeys = [ aiden gila ];
   "gila-tailscale-authkey".publicKeys = [ gila ];
   "thoth-tailscale-authkey".publicKeys = [ thoth ];
-  "cf-token.age".publicKeys = [ aiden gila ];
+  "cf-token.age".publicKeys = [ aiden gila locutus];
 }
