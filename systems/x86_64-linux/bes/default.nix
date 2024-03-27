93226733541ba8d1eb02c5244fdf4bdf2ddb0417
Aiden <aiden@oldstreetjournal.co.uk>
2024-03-27 18:23:22 +0000

    Bes: enable jellyfin and openssh

--- systems/x86_64-linux/bes/default.nix
+++ systems/x86_64-linux/bes/default.nix
@@ -1,20 +1,24 @@
 { config, inputs, lib, pkgs, systems, ... }:
 {
-  imports = [ ./hardware-configuration.nix ./disk-config.nix inputs.disko.nixosModules.default];
+  imports = [ ./hardware-configuration.nix ./disk-config.nix inputs.disko.nixosModules.default ];
 
   config = {
-
-
     # make it a module
-  services.openssh.enable = true;
-  services.openssh.openFirewall = true;
-  security.sudo.wheelNeedsPassword = false;
+    services.openssh.enable = true;
+    services.openssh.openFirewall = true;
+    security.sudo.wheelNeedsPassword = false;
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     system.stateVersion = "23.11";
     aiden.modules = {
       avahi.enabled = true;
-      jellyfin.enabled = true;
+      jellyfin = {
+        enabled = true;
+        hwAccel = {
+          enabled = true;
+          arch = "intel";
+        };
+      };
       common = {
         domainName = "bes.sw1a1aa.uk";
         enabled = true;
