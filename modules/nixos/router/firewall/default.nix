6546de99949c90beeade8fb8b2eaf3646cd5e18c
Aiden <aiden@oldstreetjournal.co.uk>
2024-04-07 20:26:31 +0100

    GL: Tighten firewall rules

--- modules/nixos/router/firewall/default.nix
+++ modules/nixos/router/firewall/default.nix
@@ -8,45 +8,7 @@ with {
       firewall.enable = false;
       nftables = {
         enable = true;
-        ruleset = ''
-          table ip filter {
-            chain input {
-              type filter hook input priority 0; policy drop;
-              ip protocol icmp counter accept comment "accept all ICMP types"
-              iifname "${externalInterface}" tcp dport { http, https } drop comment "only allow wan traffic to http{,s}"
-              iifname {"${internalInterface}", "lan", "eth3", "lo", "iot", "guest", "tailscale0"} accept comment "Allow trusted local network to access the router"
-              iifname "${externalInterface}" ct state { established, related } accept comment "Allow established traffic"
-              iifname "${externalInterface}" counter drop comment "drop all other unsolicited traffic from wan"
-            }
-
-            chain forward {
-              type filter hook forward priority 0; policy drop;
-              ip protocol icmp counter accept comment "accept all ICMP types"
-              iifname { "${internalInterface}", "lan", "guest", "eth3"} oifname { "${externalInterface}" } accept comment "Allow LAN to WAN"
-              iifname "lan" oifname "iot" accept comment "Allow lan to iot"
-              iifname "iot" oifname "lan" ct state { established, related } accept comment "Allow established from iot back to lan"
-              iifname { "${externalInterface}" } oifname { "${internalInterface}", "lan", "eth3", "guest"} ct state { established, related } accept comment "Allow established back to LANs"
-            }
-
-
-          }
-
-          table ip nat {
-            chain postrouting {
-              type nat hook postrouting priority 100; policy accept;
-              oifname "${externalInterface}" masquerade
-            }
-          }
-
-          table ip6 filter {
-          chain input {
-              type filter hook input priority 0; policy drop;
-            }
-            chain forward {
-              type filter hook forward priority 0; policy drop;
-            }
-          }
-        '';
+        ruleset = (import ./nft.nix {inherit internalInterface externalInterface;});
       };
     };
   };
