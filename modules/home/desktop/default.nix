03592bac7da64a8c3fe942b56cdc5c9dd990edb6
Aiden <aiden@oldstreetjournal.co.uk>
2024-05-22 16:51:27 +0100

    HOME: remove emacs from control

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -3,7 +3,8 @@ inputs@{ config, pkgs, lib, ... }: {
 
   xdg.enable = true;
   xdg.configFile."discord/settings.json".text = ''{"SKIP_HOST_UPDATE": true}'';
-  xdg.configFile."emacs/init.el".source = ../files/init.el;
+
+  #xdg.configFile."emacs/init.el".source = ../files/init.el;
 
   home.file."downloads".source =
     config.lib.file.mkOutOfStoreSymlink "/home/aiden/Downloads";
