34974288a4bfc0d5fcf4db3a2c54ce854511f807
Aiden <aiden@oldstreetjournal.co.uk>
2024-07-17 18:22:50 +0100

    PXE: extend pxe image

--- systems/x86_64-linux/pxe/default.nix
+++ systems/x86_64-linux/pxe/default.nix
@@ -1,4 +1,8 @@
-{ config, pkgs, lib, modulesPath, ... }: {
+{ config, pkgs, lib, modulesPath, ... }:
+let
+  publicKey = config.aiden.modules.common.publicKey;
+in
+{
   imports = [
     (modulesPath + "/installer/netboot/netboot-minimal.nix")
   ];
@@ -6,13 +10,14 @@
     users.users.root.initialPassword = "password";
     system.stateVersion = "23.11";
     aiden.modules = {
-      #avahi.enabled = true;
+      avahi.enabled = true;
       common = {
-        domainName = "pxeclient.sw1a1aa.uk";
         enabled = true;
       };
     };
     services.xserver.libinput.enable = lib.mkForce false;
 
+    users.users.nixos.openssh.authorizedKeys.keys = [publicKey];
+    users.users.root.openssh.authorizedKeys.keys = [publicKey];
   };
 }
