a4d1541d7caa55a71bf27e24cc4589b5c4882a49
Aiden <aiden@oldstreetjournal.co.uk>
2024-07-21 18:31:16 +0100

    Delete unused modules and systems including lars

--- systems/x86_64-install-iso/usb/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, inputs, ... }:
-
-{
-  aiden.modules.common.enabled = true;
-  environment.systemPackages = with pkgs; [
-    inputs.disko.packages.x86_64-linux.disko
-    git
-  ];
-}
