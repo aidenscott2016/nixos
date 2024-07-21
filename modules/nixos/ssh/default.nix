10964684a53c4cf990658441d0705dce0f40e0d8
Aiden <aiden@oldstreetjournal.co.uk>
2024-07-21 18:41:22 +0100

    Explicitly open SSH firewall port
    it's on by default though

--- modules/nixos/ssh/default.nix
+++ modules/nixos/ssh/default.nix
@@ -3,6 +3,7 @@ with lib.aiden;
 enableableModule "ssh" params {
   services.openssh = {
     enable = true;
+    openFirewall = true;
     settings = {
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
