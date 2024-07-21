6a92f68ed8e30f602bf41ddf2e685da7a0af320e
Aiden <aiden@oldstreetjournal.co.uk>
2024-07-21 14:02:48 +0100

    Enable ssh AgentForwarding on local

--- modules/home/ssh/default.nix
+++ modules/home/ssh/default.nix
@@ -5,5 +5,9 @@
   programs.ssh.enable = true;
   programs.ssh.matchBlocks."gitlab.com".identityFile = "~/.ssh/gitlab";
   programs.ssh.matchBlocks."github.com".identityFile = "~/.ssh/github";
-  programs.ssh.matchBlocks."192.168.* 10.0.* *.local *.sw1a1aa.uk".identityFile = "~/.ssh/local";
+  programs.ssh.matchBlocks."192.168.* 10.0.* *.local *.sw1a1aa.uk" = {
+    forwardAgent = true;
+    identityFile = "~/.ssh/local";
+  };
+
 }
