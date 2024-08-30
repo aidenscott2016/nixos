ec622fbfb1a01fd7468b7b69cd9b7525ea4f00ca
Aiden <aiden@oldstreetjournal.co.uk>
2024-08-30 18:35:42 +0100

    BARBIE: add i915 and autoratoation

--- systems/x86_64-linux/barbie/hardware-configuration.nix
+++ systems/x86_64-linux/barbie/hardware-configuration.nix
@@ -2,7 +2,6 @@
 # and may be overwritten by future invocations.  Please make changes
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
-
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
@@ -12,11 +11,13 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  boot.kernelParams = [
+    "i915.enable_guc=2"
+  ];
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp0s20f0u1c2.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp175s0.useDHCP = lib.mkDefault true;
@@ -24,4 +25,23 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.sensor.iio.enable = true;
+  services.udev.extraHwdb = ''
+    sensor:modalias:*
+      ACCEL_MOUNT_MATRIX=-0, -1, 0; -1, 0, 0; 0, 0, 1
+  '';
+
+
+  # extract me to own module
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs;[
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      intel-compute-runtime
+
+    ];
+    driSupport = true;
+  };
 }
