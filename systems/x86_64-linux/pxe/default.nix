221ac281fde50939f7dad7e1f434ed2259e632a5
Aiden <aiden@oldstreetjournal.co.uk>
2024-08-31 16:46:41 +0100

    PXE: remove root pass, change compression

--- systems/x86_64-linux/pxe/default.nix
+++ systems/x86_64-linux/pxe/default.nix
@@ -7,7 +7,6 @@ in
     (modulesPath + "/installer/netboot/netboot-minimal.nix")
   ];
   config = {
-    users.users.root.initialPassword = "password";
     system.stateVersion = "23.11";
     aiden.modules = {
       locale.enabled = true;
@@ -15,12 +14,13 @@ in
       common = {
         enabled = true;
       };
-      cli-base = true;
+      cli-base.enabled = true;
     };
-    services.xserver.libinput.enable = lib.mkForce false;
+    services.libinput.enable = lib.mkForce false;
 
     users.users.nixos.openssh.authorizedKeys.keys = [ publicKey ];
     users.users.root.openssh.authorizedKeys.keys = [ publicKey ];
     environment.systemPackages = with pkgs; [ rsync tmux ];
+    netboot.squashfsCompression = "zstd -Xcompression-level 1";
   };
 }
