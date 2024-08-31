87d239ec9ec4fb53754dbf2076cec4dd6650927f
Aiden <aiden@oldstreetjournal.co.uk>
2024-08-31 16:46:18 +0100

    emacs add nixfmt-classic

--- modules/nixos/emacs/default.nix
+++ modules/nixos/emacs/default.nix
@@ -1,5 +1,5 @@
 params@{ pkgs, lib, config, ... }:
 with lib.aiden;
 enableableModule "emacs" params {
-  environment.systemPackages = with pkgs; [ emacs racket ripgrep nixpkgs-fmt nodePackages.prettier ];
+  environment.systemPackages = with pkgs; [ nixfmt-classic emacs racket ripgrep nixpkgs-fmt nodePackages.prettier ];
 }
