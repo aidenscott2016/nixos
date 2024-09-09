1d45ae1407e839039175535fdf317cf469666077
Aiden <aiden@oldstreetjournal.co.uk>
2024-09-09 14:57:21 +0100

    Add doom emacs deps

--- modules/nixos/emacs/default.nix
+++ modules/nixos/emacs/default.nix
@@ -1,5 +1,16 @@
 params@{ pkgs, lib, config, ... }:
 with lib.aiden;
 enableableModule "emacs" params {
-  environment.systemPackages = with pkgs; [ nixfmt-classic emacs racket ripgrep nixpkgs-fmt nodePackages.prettier ];
+  environment.systemPackages = with pkgs; [
+    nerdfonts
+    coreutils
+    fd
+    clang
+    nixfmt-classic
+    emacs
+    racket
+    ripgrep
+    nixpkgs-fmt
+    nodePackages.prettier
+  ];
 }
