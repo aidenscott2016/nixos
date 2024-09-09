a5a2bdb1fa339a1c5244adbe6ceb66ce2e275031
Aiden <aiden@oldstreetjournal.co.uk>
2024-09-09 14:57:48 +0100

    TV: plymouth

--- systems/x86_64-linux/tv/default.nix
+++ systems/x86_64-linux/tv/default.nix
@@ -57,14 +57,37 @@ with lib.aiden;
         intel-compute-runtime
 
       ];
-    driSupport = true;
+      driSupport = true;
     };
 
-    boot.kernelParams = [
-      "i915.enable_guc=2"
-    ];
-    environment.systemPackages = with pkgs; [ firefox lm_sensors htop ];
+    environment.systemPackages = with pkgs; [ firefox lm_sensors htop moonlight-qt];
 
     services.logrotate.checkConfig = false;
+    boot = {
+      plymouth = {
+        enable = true;
+        theme = "rings";
+        themePackages = with pkgs; [
+          # By default we would install all themes
+          (adi1090x-plymouth-themes.override {
+            selected_themes = [ "rings" ];
+          })
+        ];
+      };
+      # Enable "Silent Boot"
+      consoleLogLevel = 0;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "loglevel=3"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+
+        "i915.enable_guc=2"
+      ];
+    };
   };
 }
