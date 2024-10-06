6e53d695271c7f6f8673413673290f01642d151d
Aiden <aiden@oldstreetjournal.co.uk>
2024-10-06 19:26:21 +0100

    bes: add beets

--- systems/x86_64-linux/bes/default.nix
+++ systems/x86_64-linux/bes/default.nix
@@ -8,82 +8,128 @@
     ./portainer.nix
   ];
 
-  config = {
-    services.openssh.enable = true;
-    services.openssh.openFirewall = true;
-    security.sudo.wheelNeedsPassword = false;
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-    system.stateVersion = "23.11";
+  config =
 
-    services.deluge = { enable = true; web = { enable = true; port = 8112; }; };
-    users.users.deluge.extraGroups = [ "video" ];
+    {
+      services.openssh.enable = true;
+      services.openssh.openFirewall = true;
+      security.sudo.wheelNeedsPassword = false;
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+      system.stateVersion = "23.11";
 
-    services.bazarr = {
-      enable = true;
-      group = "video";
-    };
-    users.users.bazarr.extraGroups = [ "video" ];
-
-    services.sonarr = {
-      enable = true;
-      group = "video";
-    };
-    users.users.sonarr.extraGroups = [ "video" ];
-
-    services.radarr = {
-      enable = true;
-      group = "video";
-    };
-    users.users.radarr.extraGroups = [ "video" ];
-
-    services.sabnzbd = {
-      enable = true; # configFile = config.age.secrets.sabnzbd.path;
-      group = "video";
-    };
-    users.users.sabnzbd.extraGroups = [ "video" ];
-
-    users.users.aiden.extraGroups = [ "video" "sadnzbd" "deluge" ];
-
-    networking.firewall.allowedTCPPorts = [ 443 5000 ];
-
-    aiden.modules = {
-      gc.enabled = false;
-      cli-base.enabled = true;
-      locale.enabled = true;
-      reverseProxy = {
-        enabled = true;
-        apps = [
-          { name = "bazarr"; port = 6767; }
-          { name = "sonarr"; port = 8989; }
-          { name = "sab"; port = 8080; }
-          { name = "jellyfin"; port = 8096; }
-          { name = "portainer"; port = 9000; }
-          { name = "deluge"; port = 8112; }
-          { name = "radarr"; port = 7878; }
-        ];
+      services.deluge = {
+        enable = true;
+        web = {
+          enable = true;
+          port = 8112;
+        };
       };
-      avahi.enabled = true;
-      jellyfin = {
-        enabled = true;
-        hwAccel = {
+      users.users.deluge.extraGroups = [ "video" ];
+
+      services.bazarr = {
+        enable = true;
+        group = "video";
+      };
+      users.users.bazarr.extraGroups = [ "video" ];
+
+      services.sonarr = {
+        enable = true;
+        group = "video";
+      };
+      users.users.sonarr.extraGroups = [ "video" ];
+
+      services.radarr = {
+        enable = true;
+        group = "video";
+      };
+      users.users.radarr.extraGroups = [ "video" ];
+
+      services.sabnzbd = {
+        enable = true; # configFile = config.age.secrets.sabnzbd.path;
+        group = "video";
+      };
+      users.users.sabnzbd.extraGroups = [ "video" ];
+
+      users.users.aiden.extraGroups = [ "video" "sadnzbd" "deluge" ];
+
+      networking.firewall.allowedTCPPorts = [ 443 5000 ];
+
+      aiden.modules = {
+        gc.enabled = false;
+        cli-base.enabled = true;
+        locale.enabled = true;
+        reverseProxy = {
           enabled = true;
-          arch = "intel";
+          apps = [
+            {
+              name = "bazarr";
+              port = 6767;
+            }
+            {
+              name = "sonarr";
+              port = 8989;
+            }
+            {
+              name = "sab";
+              port = 8080;
+            }
+            {
+              name = "jellyfin";
+              port = 8096;
+            }
+            {
+              name = "portainer";
+              port = 9000;
+            }
+            {
+              name = "deluge";
+              port = 8112;
+            }
+            {
+              name = "radarr";
+              port = 7878;
+            }
+          ];
+        };
+        avahi.enabled = true;
+        jellyfin = {
+          enabled = true;
+          hwAccel = {
+            enabled = true;
+            arch = "intel";
+          };
+        };
+        common = {
+          domainName = "bes.sw1a1aa.uk";
+          enabled = true;
+        };
+        samba = {
+          enabled = true;
+          shares.t7 = {
+            path = "/media/t7";
+            writable = "true";
+          };
         };
       };
-      common = {
-        domainName = "bes.sw1a1aa.uk";
-        enabled = true;
-      };
-      samba = {
-        enabled = true;
-        shares.t7 = {
-          path = "/media/t7";
-          writable = "true";
-        };
-      };
-    };
-    environment.systemPackages = with pkgs; [ get_iplayer wol iperf3 ];
+      environment.systemPackages = with pkgs; [
+        get_iplayer
+        wol
+        iperf3
 
-  };
+        (beets-unstable.override {
+          pluginOverrides = {
+            #fetchart
+            #badfiles.enable = true;
+            discogs.enable = true;
+            copyartifacts = {
+              enable = true;
+              propagatedBuildInputs = [ beetsPackages.copyartifacts ];
+            };
+          };
+        })
+
+      ];
+
+    };
 }
