741b495b1c88183389c7b905abe9ff6fb563b47b
Aiden <aiden@oldstreetjournal.co.uk>
2024-11-13 08:01:08 +0000

    Start HASS after traefik
    So that traefik docker adaptor will pick it up

--- modules/nixos/home-assistant/default.nix
+++ modules/nixos/home-assistant/default.nix
@@ -6,23 +6,28 @@ let
   enabled = config.aiden.modules.home-assistant.enabled;
   inherit (config.aiden.modules.common) domainName email;
   fqdn = "hass.${domainName}";
-in
-{
+  container-name = "home-assistant";
+  service-name =
+    "${config.virtualisation.oci-containers.backend}-${container-name}";
+in {
   options.aiden.modules.home-assistant = {
     enabled = mkEnableOption "home-assistant";
     devices = mkOption {
       type = with types; listOf str;
-      example = [ "/dev/serial/by-id/usb-Nabu_Casa_SkyConnect_v1.0_2ee577279f96ed119403c098a7669f5d-if00-port0" ];
+      example = [
+        "/dev/serial/by-id/usb-Nabu_Casa_SkyConnect_v1.0_2ee577279f96ed119403c098a7669f5d-if00-port0"
+      ];
       description = "A list of devices to mount in the HA image";
       default = [ ];
     };
   };
   config = mkIf enabled {
+    systemd.services.${service-name}.after = [ "traefik.service" ];
     networking.hosts."10.0.1.1" = [ fqdn ];
 
     virtualisation.oci-containers = {
       backend = "podman";
-      containers.homeassistant = {
+      containers.${container-name} = {
         volumes = [ "home-assistant:/config" ];
         environment.TZ = "Europe/London";
         labels = {
@@ -33,11 +38,8 @@ in
           "traefik.http.services.hass.loadbalancer.server.port" = "8123";
           "traefik.http.routers.hass.entrypoints" = "websecure";
         };
-        image =
-          "ghcr.io/home-assistant/home-assistant:stable";
-        extraOptions = [
-          "--network=host"
-        ] ++ deviceParams;
+        image = "ghcr.io/home-assistant/home-assistant:stable";
+        extraOptions = [ "--network=host" ] ++ deviceParams;
       };
     };
 
