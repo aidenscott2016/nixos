b293cfa1c02504d077799429c316257f4934dd7f
Aiden <aiden@oldstreetjournal.co.uk>
2025-03-09 14:09:50 +0000

    PXE: add facter package

--- systems/x86_64-linux/pxe/default.nix
+++ systems/x86_64-linux/pxe/default.nix
@@ -1,26 +1,20 @@
 { config, pkgs, lib, modulesPath, ... }:
-let
-  publicKey = config.aiden.modules.common.publicKey;
-in
-{
-  imports = [
-    (modulesPath + "/installer/netboot/netboot-minimal.nix")
-  ];
+let publicKey = config.aiden.modules.common.publicKey;
+in {
+  imports = [ (modulesPath + "/installer/netboot/netboot-minimal.nix") ];
   config = {
     system.stateVersion = "23.11";
     aiden.modules = {
       locale.enabled = true;
       avahi.enabled = true;
-      common = {
-        enabled = true;
-      };
+      common = { enabled = true; };
       cli-base.enabled = true;
     };
     services.libinput.enable = lib.mkForce false;
 
     users.users.nixos.openssh.authorizedKeys.keys = [ publicKey ];
     users.users.root.openssh.authorizedKeys.keys = [ publicKey ];
-    environment.systemPackages = with pkgs; [ rsync tmux ];
+    environment.systemPackages = with pkgs; [ rsync tmux nixos-facter ];
     netboot.squashfsCompression = "zstd -Xcompression-level 1";
   };
 }
