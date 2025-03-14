45a3b77a7f726edec8e20fc1489560285142d2a7
Aiden <aiden@oldstreetjournal.co.uk>
2025-03-14 13:24:02 +0000

    add bitwarden

--- systems/x86_64-linux/mike/packages.nix
+++ systems/x86_64-linux/mike/packages.nix
@@ -1,29 +1,18 @@
 params@{ config, pkgs, inputs, channels, lib, ... }: {
   environment.systemPackages = with pkgs; [
-    imagemagick
     powertop
     acpi # seems to provide more accurate charging status than upower. see the underpowered anker charger
     (discord.override { withTTS = false; })
-    #jetbrains.idea-community
     chromium
     inputs.agenix.packages.x86_64-linux.default
     dnsutils # nslookup
     silver-searcher
     transmission-gtk
     nixpkgs-fmt
-    rustc
-    cargo
     xclip
-    ventoy-full
-    # minecraft
-    # prismlauncher
     libva-utils
     xorg.xev
-    get_iplayer
     wol
-    # hledger
-    # haskellPackages.hledger-flow
-
     vlc
     podman-compose
     docker-compose
@@ -36,7 +25,7 @@ params@{ config, pkgs, inputs, channels, lib, ... }: {
     okular
     kubectl
     yubikey-manager
-    virtualbox
+    bitwarden-desktop
   ];
 
 }
