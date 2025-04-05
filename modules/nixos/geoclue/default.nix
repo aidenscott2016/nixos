f8871fd120256c55982b7158e689f5e8fdb18ec6
Aiden <aiden@oldstreetjournal.co.uk>
2025-04-05 16:50:28 +0100

    add geoclue module

---
+++ modules/nixos/geoclue/default.nix
@@ -0,0 +1,67 @@
+params@{ lib, pkgs, config, ... }:
+with lib;
+let moduleName = "geoclue";
+in {
+  options = {
+    aiden.modules.geoclue.enabled = mkEnableOption moduleName;
+    aiden.modules.geoclue.apps = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          isAllowed = mkOption {
+            type = types.bool;
+            default = false;
+          };
+          isSystem = mkOption {
+            type = types.bool;
+            default = false;
+          };
+        };
+      });
+      default = { };
+      description = "Applications that need geolocation access";
+    };
+    aiden.modules.geoclue.staticLocation = mkOption {
+      type = types.submodule {
+        options = {
+          latitude = mkOption {
+            type = types.float;
+            default = 51;
+          };
+          longitude = mkOption {
+            type = types.float;
+            default = -1;
+          };
+        };
+      };
+      default = { };
+      description =
+        "Static location configuration when WiFi-based location is disabled";
+    };
+  };
+
+  config = mkIf config.aiden.modules.geoclue.enabled {
+    services.geoclue2 = {
+      enable = true;
+      enableWifi = false;
+      appConfig = config.aiden.modules.geoclue.apps;
+    };
+
+    environment.etc = lib.mkIf (!config.services.geoclue2.enableWifi) {
+      "geolocation".text = ''
+        ${
+          toString config.aiden.modules.geoclue.staticLocation.latitude
+        }   # latitude
+        ${
+          toString config.aiden.modules.geoclue.staticLocation.longitude
+        }  # longitude
+        96           # altitude
+        1.83         # accuracy radius
+      '';
+
+      "geoclue/conf.d/00-config.conf".text = ''
+        [static-source]
+        enable=true
+      '';
+    };
+  };
+}
