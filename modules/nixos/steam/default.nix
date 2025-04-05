bf42cbb9d920c9d54352ead3f5708acf4604e5cf
Aiden <aiden@oldstreetjournal.co.uk>
2025-04-05 13:50:50 +0100

    Add steamtinkerlaunch as systempackage

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -9,16 +9,10 @@ in enableableModule "steam" params {
   programs.steam = {
     enable = true;
     protontricks.enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
     extraCompatPackages = [ steamtinkerlaunch-git ];
+    environment.systemPackages = [ steamtinkerlaunch-git ];
   };
   programs.gamemode.enable = true;
   programs.gamemode.enableRenice = true;
-  environment.systemPackages = [
-    #steamtinkerlaunch
-    python312Packages.ds4drv
-    python3
-  ];
-
 }
