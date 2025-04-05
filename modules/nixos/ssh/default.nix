fc5cb71c1d8692774aca8ec07e9302782a565947
Aiden <aiden@oldstreetjournal.co.uk>
2025-04-05 13:51:13 +0100

    SSH: add iphone public key

--- modules/nixos/ssh/default.nix
+++ modules/nixos/ssh/default.nix
@@ -11,5 +11,6 @@ enableableModule "ssh" params {
   };
   users.users.aiden.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgHxgT0rlJDXl+opb7o2JSfjd5lJZ6QTRr57N0MIAyN aiden@lars"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCoq4Vfco724r3Ogg0s2fijnu9GtDsDW/e5JsKAQOzf"
   ];
 }
