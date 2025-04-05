46336995620d18217f42627a97e35e95373a760b
Aiden <aiden@oldstreetjournal.co.uk>
2025-04-05 16:50:56 +0100

    use geoclue module in darkman, redshift

--- modules/nixos/darkman/default.nix
+++ modules/nixos/darkman/default.nix
@@ -4,13 +4,20 @@ let moduleName = "darkman";
 in {
   options = { aiden.modules.darkman.enabled = mkEnableOption moduleName; };
   config = mkIf config.aiden.modules.darkman.enabled {
-
     environment.pathsToLink =
       [ "/share/xdg-desktop-portal" "/share/applications" ];
 
+    aiden.modules.geoclue = {
+      enabled = true;
+      apps.darkman = {
+        isAllowed = true;
+        isSystem = true;
+      };
+    };
+
+    # The darkman service comes from home-manager
     home-manager.users.aiden = {
-      aiden.modules."${moduleName}".enabled = true;
+      aiden.modules.darkman.enabled = true;
     };
   };
-
 }
--- modules/nixos/redshift/default.nix
+++ modules/nixos/redshift/default.nix
@@ -6,16 +6,4 @@ enableableModule "redshift" params {
     geoclue2.enable = true;
   };
   location.provider = "geoclue2";
-  environment.etc = lib.mkIf (!config.services.geoclue2.enableWifi) {
-    "geolocation".text = ''
-      51.064148701061185   # latitude
-      -1.3189842441933493  # longitude
-      96           # altitude
-      1.83         # accuracy radius
-    '';
-    "geoclue/conf.d/00-config.conf".text = ''
-      [static-source]
-      enable=true
-    '';
-  };
 }
