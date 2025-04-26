57dbfd4bbe1daa85ffd0a9d597975d5d447bf4bf
Aiden <aiden@oldstreetjournal.co.uk>
2025-04-26 15:20:25 +0100

    update inputs

--- flake.nix
+++ flake.nix
@@ -5,16 +5,23 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
-    nur.url = "github:nix-community/NUR";
-    firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-    microvm.url = "github:astro/microvm.nix";
-    microvm.inputs.nixpkgs.follows = "nixpkgs";
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    microvm = {
+      url = "github:astro/microvm.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     steamtinkerlaunch = {
       flake = false;
       url = "github:sonic2kk/steamtinkerlaunch";
     };
     dwm = {
-
       url = "github:aidenscott2016/dwm";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -40,17 +47,25 @@
     nixos-facter.url = "github:nix-community/nixos-facter";
     nixos-images.url = "github:nix-community/nixos-images";
   };
-  outputs = inputs:
+  outputs =
+    inputs:
     inputs.snowfall-lib.mkFlake {
       inherit inputs;
       src = ./.;
-      snowfall = { namespace = "aiden"; };
-      diskoConfigurations = { locutus = import ./hosts/locutus/disko.nix; };
+      snowfall = {
+        namespace = "aiden";
+      };
+      diskoConfigurations = {
+        locutus = import ./hosts/locutus/disko.nix;
+      };
       channels-config = {
+        packageOverrides = pkgs: { firefox-addons = inputs.firefox-addons { inherit pkgs; }; };
         nvidia.acceptLicense = true;
         allowUnfree = true;
-        config.permittedInsecurePackages =
-          [ "aspnetcore-runtime-wrapped-6.0.36" "aspnetcore-runtime-6.0.36" ];
+        config.permittedInsecurePackages = [
+          "aspnetcore-runtime-wrapped-6.0.36"
+          "aspnetcore-runtime-6.0.36"
+        ];
 
       };
     };
