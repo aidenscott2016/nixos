5504d22a3b62878af41176e737c30df9b675632f
Aiden <aiden@oldstreetjournal.co.uk>
2025-04-26 15:22:31 +0100

    add gamemode settings

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -1,18 +1,46 @@
-params@{ pkgs, lib, config, inputs, ... }:
+params@{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 with lib.aiden;
 with pkgs;
 let
-  steamtinkerlaunch-git = pkgs.steamtinkerlaunch.overrideAttrs
-    (_: { src = inputs.steamtinkerlaunch; });
-in enableableModule "steam" params {
+  steamtinkerlaunch-git = pkgs.steamtinkerlaunch.overrideAttrs (_: {
+    src = inputs.steamtinkerlaunch;
+  });
+in
+enableableModule "steam" params {
   programs.steam = {
     enable = true;
     protontricks.enable = true;
     gamescopeSession.enable = true;
     extraCompatPackages = [ steamtinkerlaunch-git ];
   };
-  environment.systemPackages = [ steamtinkerlaunch-git ];
+  environment.systemPackages = [
+    steamtinkerlaunch-git
+  ];
   programs.gamemode.enable = true;
   programs.gamemode.enableRenice = true;
+  programs.gamemode.settings = {
+    general = {
+      renice = 10;
+    };
+
+    # Warning: GPU optimisations have the potential to damage hardware
+    gpu = {
+      apply_gpu_optimisations = "accept-responsibility";
+      gpu_device = 0;
+      nv_powermizer_mode = 1;
+    };
+
+    custom = {
+      start = "''${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+      end = "''${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+    };
+  };
+  users.users.aiden.extraGroups = [ "gamemode" ];
 }
--- modules/nixos/xdg-portal/default.nix
+++ modules/nixos/xdg-portal/default.nix
@@ -15,6 +15,7 @@ in
 
   config = mkIf cfg.enable {
     xdg.portal = {
+      enable = true;
       config.common = {
         default = "*";
       };
