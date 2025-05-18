724afccc4181547e815360146492963f6534c6a6
Aiden <aiden@oldstreetjournal.co.uk>
2025-05-18 11:00:52 +0100

    adds tlp config for muli battery

--- modules/nixos/powermanagement/default.nix
+++ modules/nixos/powermanagement/default.nix
@@ -1,16 +1,30 @@
-params@{ pkgs, lib, config, ... }:
+params@{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 with lib;
-let cfg = config.aiden.modules.powermanagement;
-in {
+let
+  cfg = config.aiden.modules.powermanagement;
+in
+{
   options.aiden.modules.powermanagement = {
     enable = mkEnableOption "powermanagement";
   };
   config = mkIf cfg.enable {
+    services.auto-cpufreq.enable = false;
     services.tlp = {
       enable = true;
       settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+
         START_CHARGE_THRESH_BAT0 = 75;
         STOP_CHARGE_THRESH_BAT0 = 80;
+        START_CHARGE_THRESH_BAT1 = 75;
+        STOP_CHARGE_THRESH_BAT1 = 80;
       };
     };
   };
