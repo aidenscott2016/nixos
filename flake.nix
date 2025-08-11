f2ec09f399fbc5795adb301246b094dff98901be
Aiden <aiden@oldstreetjournal.co.uk>
2025-08-11 07:57:53 +0100

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -13,7 +13,9 @@
     ];
   };
   inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/release-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager.url = "github:nix-community/home-manager/master";
