1490d2da2a5770e8c08fe7bede83b89060d6934d
Aiden <aiden@oldstreetjournal.co.uk>
2025-09-29 08:21:47 +0100

    set proxy settings for paperless

--- modules/nixos/paperles/default.nix
+++ modules/nixos/paperles/default.nix
@@ -18,6 +18,13 @@ in
       enable = true;
       settings = {
         PAPERLESS_URL = "https://paperless.sw1a1aa.uk";
+        PAPERLESS_USE_X_FORWARD_HOST = false;
+
+        # Paperless will return http urls for `next` fiels in paginated API repsonsed without this
+        PAPERLESS_PROXY_SSL_HEADER = [
+          "HTTP_X_FORWARDED_PROTO"
+          "https"
+        ];
       };
     };
     aiden.modules.reverseProxy.apps = [
