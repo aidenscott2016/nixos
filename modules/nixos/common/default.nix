78b07a9c08ec6357ccef6013f556796e458e870b
Aiden <aiden@oldstreetjournal.co.uk>
2025-09-29 08:22:21 +0100

    add cachinx

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -26,6 +26,13 @@ in
     nix.extraOptions = "experimental-features = nix-command flakes";
     nix.settings.auto-optimise-store = true;
     nix.settings.trusted-users = [ "aiden" ];
+    nix.settings.substituters = [
+      "https://nix-community.cachix.org"
+    ];
+
+    nix.settings.trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
 
     users.users.aiden = {
       uid = 1000;
