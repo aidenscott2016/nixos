c398df22edf3c9c48239bb7f7e420e9f8337b960
Aiden <aiden@oldstreetjournal.co.uk>
2025-11-05 06:48:42 +0000

    add caches

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -44,10 +44,13 @@ in
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://cache.flox.dev"
       ];
+
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0mWqyPMV+FnfCelaCYkFdeVX6Ht7cg="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
       ];
     };
   };
