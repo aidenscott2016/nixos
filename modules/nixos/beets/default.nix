5b2e5f8e2265a016c4c5f7d89173eebb8b225cef
Aiden <aiden@oldstreetjournal.co.uk>
2025-12-24 16:14:00 +0000

    add back old beets override as a comment

--- modules/nixos/beets/default.nix
+++ modules/nixos/beets/default.nix
@@ -6,6 +6,24 @@ _@{
   ...
 }:
 with lib;
+# let
+#   beet-override =
+#     with pkgs;
+#     (beets.override {
+#       pluginOverrides = {
+#         fetchart.enable = true;
+#         bandcamp = {
+#           enable = true;
+#           propagatedBuildInputs = [ inputs.self.packages.x86_64-linux.beetcamp ];
+#         };
+#         discogs.enable = true;
+#         copyartifacts = {
+#           enable = true;
+#           propagatedBuildInputs = [ beetsPackages.copyartifacts ];
+#         };
+#       };
+#     });
+# in
 {
   options = {
     aiden.programs.beets.enable = mkEnableOption "beets";
