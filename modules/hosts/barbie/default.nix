22ee0302fbb69a3103b6a48700d80cc7a484314e
Aiden <aiden@oldstreetjournal.co.uk>
2025-12-28 20:33:26 +0000

    refactor(barbie): transform host to den pattern
    Applied den pattern transformation to barbie host configuration.

--- modules/hosts/barbie/default.nix
+++ modules/hosts/barbie/default.nix
@@ -1,68 +1,115 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{ den, nd, inputs, ... }: {
+  # Host declaration with home-manager integration
+  den.hosts.x86_64-linux.barbie.users.aiden = {};
 
-{ inputs, ... }:
-{
-  flake.nixosConfigurations.barbie = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    specialArgs = { inherit inputs; };
-    modules = [
-      ../../../modules/nixos/avahi/default.nix
-      ../../../modules/nixos/common/default.nix
-      ../../../modules/nixos/ssh/default.nix
-      ../../../modules/nixos/locale/default.nix
-      ({ config, lib, pkgs, inputs, ... }: {
-        imports = [
-          inputs.disko.nixosModules.default
-          inputs.home-manager.nixosModules.home-manager
-          ./disk-configuration.nix
-          ./hardware-configuration.nix
-          inputs.nixos-hardware.nixosModules.gpd-pocket-3
-        ];
-
-        # Use the systemd-boot EFI boot loader.
-        boot.loader.systemd-boot.enable = true;
-        boot.loader.efi.canTouchEfiVariables = true;
-
-        networking.hostName = "barbie";
-        networking.networkmanager.enable = true;
-        # Enable the X11 windowing system.
-        services.xserver.enable = true;
-        services.pipewire = {
-          enable = true;
-          pulse.enable = true;
-        };
-
-        services.openssh.openFirewall = true;
-        services.desktopManager.plasma6.enable = true;
-
-        security.sudo.wheelNeedsPassword = false; # desktop archetype
-
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = { inherit inputs; };
-        home-manager.users.aiden = {
-          imports = [
-            ../../../modules/home/bash/default.nix
-            ../../../modules/home/darkman/default.nix
-            ../../../modules/home/desktop/default.nix
-            ../../../modules/home/easyeffects/default.nix
-            ../../../modules/home/firefox/default.nix
-            ../../../modules/home/git/default.nix
-            ../../../modules/home/gpg-agent/default.nix
-            ../../../modules/home/ideavim/default.nix
-            ../../../modules/home/ssh/default.nix
-            ../../../modules/home/tmux/default.nix
-            ../../../modules/home/vim/default.nix
-            ../../../modules/home/xdg-portal/default.nix
-          ];
-        };
-        system.stateVersion = "24.05"; # Did you read the comment?
-        environment.systemPackages = [
-          pkgs.maliit-keyboard
-        ];
-      })
+  # Host aspect
+  den.aspects.barbie = {
+    includes = [
+      nd.common
+      nd.ssh
+      nd.locale
+      nd.avahi
+      nd.barbie-hardware
     ];
+
+    nixos = { config, pkgs, lib, ... }: {
+      imports = [
+        inputs.disko.nixosModules.default
+        inputs.nixos-hardware.nixosModules.gpd-pocket-3
+      ];
+
+      # Disko configuration
+      disko.devices = {
+        disk = {
+          vdb = {
+            type = "disk";
+            device = "/dev/nvme0n1";
+            content = {
+              type = "gpt";
+              partitions = {
+                ESP = {
+                  size = "500M";
+                  type = "EF00";
+                  content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                    mountOptions = [ "defaults" ];
+                  };
+                };
+                luks = {
+                  size = "100%";
+                  content = {
+                    type = "luks";
+                    name = "crypted";
+                    content = {
+                      type = "lvm_pv";
+                      vg = "pool";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+        lvm_vg = {
+          pool = {
+            type = "lvm_vg";
+            lvs = {
+              root = {
+                size = "75G";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                  mountOptions = [ "defaults" ];
+                };
+              };
+              swap = {
+                size = "8G";
+                content = { type = "swap"; };
+              };
+              home = {
+                size = "+100%FREE";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/home";
+                };
+              };
+              raw = {
+                size = "10M";
+              };
+            };
+          };
+        };
+      };
+
+      # Host-specific config
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+
+      networking.hostName = "barbie";
+      networking.networkmanager.enable = true;
+
+      services.xserver.enable = true;
+      services.pipewire = {
+        enable = true;
+        pulse.enable = true;
+      };
+      services.openssh.openFirewall = true;
+      services.desktopManager.plasma6.enable = true;
+
+      security.sudo.wheelNeedsPassword = false;
+      system.stateVersion = "24.05";
+
+      environment.systemPackages = [ pkgs.maliit-keyboard ];
+
+      # Module options (using new namespace)
+      narrowdivergent.aspects.common = {
+        domainName = "narrowdivergent.com";
+        email = "aiden@narrowdivergent.com";
+      };
+    };
   };
 }
