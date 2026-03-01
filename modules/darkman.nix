0c3cf46ba54222f0f3c1fd78689fa42c2da82159
Aiden <aiden@oldstreetjournal.co.uk>
2026-03-01 18:28:01 +0000

    refactor: unify cross-cutting NixOS+HM modules
    Merge NixOS and HM sides for darkman (new file) and ssh (add HM side).
    xdg-portal.nix already had darkman-specific lines removed in w2-nixos-batch2.

---
+++ modules/darkman.nix
@@ -0,0 +1,34 @@
+{ ... }:
+{
+  flake.modules.nixos.darkman =
+    { lib, pkgs, config, ... }:
+    with lib;
+    {
+      environment.pathsToLink = [
+        "/share/xdg-desktop-portal"
+        "/share/applications"
+      ];
+      aiden.modules.geoclue = {
+        apps.darkman = {
+          isAllowed = true;
+          isSystem = true;
+        };
+      };
+    };
+
+  flake.modules.homeManager.darkman =
+    { lib, pkgs, config, ... }:
+    with lib;
+    {
+      xdg.portal = {
+        config.common = {
+          "org.freedesktop.impl.portal.Settings" = [ "darkman" ];
+        };
+        extraPortals = [ pkgs.darkman ];
+      };
+      services.darkman = {
+        enable = true;
+        settings.usegeoclue = true;
+      };
+    };
+}
--- modules/ssh.nix
+++ modules/ssh.nix
@@ -3,17 +3,43 @@
   flake.modules.nixos.ssh =
     { pkgs, lib, config, ... }:
     {
-        services.openssh = {
-          enable = true;
-          openFirewall = true;
-          settings = {
-            PasswordAuthentication = false;
-            KbdInteractiveAuthentication = false;
+      services.openssh = {
+        enable = true;
+        openFirewall = true;
+        settings = {
+          PasswordAuthentication = false;
+          KbdInteractiveAuthentication = false;
+        };
+      };
+      users.users.aiden.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgHxgT0rlJDXl+opb7o2JSfjd5lJZ6QTRr57N0MIAyN aiden@lars"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCoq4Vfco724r3Ogg0s2fijnu9GtDsDW/e5JsKAQOzf"
+      ];
+    };
+
+  flake.modules.homeManager.ssh =
+    { config, pkgs, lib, ... }:
+    {
+      programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
+        matchBlocks = {
+          "gitlab.com".identityFile = "~/.ssh/gitlab";
+          "github.com".identityFile = "~/.ssh/github";
+          "192.168.* 10.0.* *.local *.sw1a1aa.uk" = {
+            forwardAgent = true;
+            identityFile = "~/.ssh/local";
+          };
+          "192.168.122.*".extraOptions = {
+            "StrictHostKeyChecking" = "no";
+            "UserKnownHostsFile" = "/dev/null";
+          };
+          "*" = {
+            compression = true;
+            serverAliveInterval = 30;
+            serverAliveCountMax = 3;
           };
         };
-        users.users.aiden.openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgHxgT0rlJDXl+opb7o2JSfjd5lJZ6QTRr57N0MIAyN aiden@lars"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCoq4Vfco724r3Ogg0s2fijnu9GtDsDW/e5JsKAQOzf"
-        ];
+      };
     };
 }
