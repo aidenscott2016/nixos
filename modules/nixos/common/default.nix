b2d0560779a36f8dfca82bfa7709677ec9b4f0df
Aiden <aiden@oldstreetjournal.co.uk>
2026-03-01 17:39:30 +0000

    set fish as default shell for aiden user

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -34,10 +34,13 @@ in
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
 
+    programs.fish.enable = true;
+
     users.users.aiden = {
       uid = 1000;
       initialPassword = "password";
       isNormalUser = true;
+      shell = pkgs.fish;
       extraGroups = [
         "wheel"
         "disk"
