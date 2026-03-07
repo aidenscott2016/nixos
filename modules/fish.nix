79695b93c23aed74b85ec0a0d9ba71e7561770bb
Aiden <aiden@oldstreetjournal.co.uk>
2026-03-07 23:05:31 +0000

    fish: Set nim prompt

--- modules/fish.nix
+++ modules/fish.nix
@@ -5,7 +5,10 @@
     {
       programs.fish = {
         enable = true;
-        interactiveShellInit = "fish_vi_key_bindings";
+        interactiveShellInit = ''
+          fish_vi_key_bindings
+          source $__fish_data_dir/tools/web_config/sample_prompts/nim.fish
+        '';
       };
     };
 }
