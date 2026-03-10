3dfb6e157ed2a6200cfdb17059925627005af923
Aiden <aiden@oldstreetjournal.co.uk>
2026-03-10 07:30:18 +0000

    nix: Enable aarch64 binfmt emulation on x86_64 hosts
    Without this, building aarch64 configurations like lovelace from an
    x86_64 machine fails because NixOS system-assembly derivations need
    a builder for the target architecture.

--- modules/nix.nix
+++ modules/nix.nix
@@ -14,6 +14,9 @@
         inputs.disko.packages.${pkgs.stdenv.hostPlatform.system}.disko
       ];
 
+      boot.binfmt.emulatedSystems =
+        lib.optionals (pkgs.stdenv.hostPlatform.system == "x86_64-linux") [ "aarch64-linux" ];
+
       nix.settings = {
         substituters = [ "https://cache.flox.dev" ];
         trusted-public-keys = [
