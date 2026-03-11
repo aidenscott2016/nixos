4509b61f903ffb10c8f05b3f851dac9c9133a974
Aiden <aiden@oldstreetjournal.co.uk>
2026-03-11 06:36:43 +0000

    adguard: Use host/port options instead of settings.http.address
    The NixOS adguardhome module has dedicated host and port options that
    override settings.http.address when generating the runtime YAML.
    Setting the address via settings was silently ignored, causing AdGuard
    to bind to 0.0.0.0:3000 while Traefik expected 10.0.1.1:8081.

--- modules/adguard.nix
+++ modules/adguard.nix
@@ -3,15 +3,15 @@
   flake.modules.nixos.adguard =
     { pkgs, lib, config, ... }:
     let
-      http_port = 8081;
       inherit (config.aiden.modules.common) domainName email;
       fqdn = "adguard.${domainName}";
     in
     {
         services.adguardhome = {
           enable = true;
+          host = "10.0.1.1";
+          port = 8081;
           settings = {
-            http.address = "10.0.1.1:8081";
             dns.bind_hosts = [ "127.0.0.2" ];
             dns.port = 5354; # mdns uses 5353
             users = [
