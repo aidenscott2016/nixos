9a2795d7e27ca2766b25bdd6e5a61ec3977f488b
Aiden <aiden@oldstreetjournal.co.uk>
2026-03-11 07:40:11 +0000

    bes: add opencode

--- modules/hosts/bes/bes.nix
+++ modules/hosts/bes/bes.nix
@@ -8,100 +8,148 @@
       ./_portainer.nix
       inputs.agenix.nixosModules.default
       inputs.disko.nixosModules.disko
-    ] ++ (with config.flake.modules.nixos; [
-      common architecture locale avahi syncthing navidrome jellyfin paperless media-storage beets
-    ]) ++ [
+    ]
+    ++ (with config.flake.modules.nixos; [
+      common
+      architecture
+      locale
+      avahi
+      syncthing
+      navidrome
+      jellyfin
+      paperless
+      media-storage
+      beets
+    ])
+    ++ [
       config.flake.modules.nixos."cli-base"
       config.flake.modules.nixos."reverse-proxy"
-    ] ++ [
-      ({ config, pkgs, lib, ... }: {
-        networking.hostName = "bes";
-        system.stateVersion = "23.11";
-        nixpkgs.overlays = [ inputs.self.overlays.default ];
+    ]
+    ++ [
+      (
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
+        {
+          networking.hostName = "bes";
+          system.stateVersion = "23.11";
+          nixpkgs.overlays = [ inputs.self.overlays.default ];
 
-        aiden = {
-          architecture = {
-            cpu = "intel";
-            gpu = "intel";
-          };
-          modules = {
-            common.domainName = "bes.sw1a1aa.uk";
-            reverseProxy.apps = [
-              { name = "bazarr"; port = 6767; }
-              { name = "sonarr"; port = 8989; }
-              { name = "sab"; port = 8080; }
-              { name = "jellyfin"; port = 8096; }
-              { name = "portainer"; port = 9000; }
-              { name = "deluge"; port = 8112; }
-              { name = "radarr"; port = 7878; }
-              { name = "slskd"; port = 5030; }
-            ];
-          };
-        };
-
-        age.secrets.slskd.file = "${inputs.self.outPath}/secrets/slskd";
-
-        services.iperf3.enable = true;
-        services.iperf3.openFirewall = true;
-        services.openssh.enable = true;
-        services.openssh.openFirewall = true;
-        security.sudo.wheelNeedsPassword = false;
-        boot.loader.systemd-boot.enable = true;
-        boot.loader.efi.canTouchEfiVariables = true;
-
-        services.cockpit.enable = true;
-        services.cockpit.openFirewall = true;
-
-        services.slskd = {
-          enable = true;
-          domain = null;
-          settings = {
-            shares.directories = [ "/srv/media/Music" ];
-            directories = {
-              incomplete = "/srv/media/Music/download/incomplete";
-              downloads = "/srv/media/Music/download/complete";
+          aiden = {
+            architecture = {
+              cpu = "intel";
+              gpu = "intel";
+            };
+            modules = {
+              common.domainName = "bes.sw1a1aa.uk";
+              reverseProxy.apps = [
+                {
+                  name = "bazarr";
+                  port = 6767;
+                }
+                {
+                  name = "sonarr";
+                  port = 8989;
+                }
+                {
+                  name = "sab";
+                  port = 8080;
+                }
+                {
+                  name = "jellyfin";
+                  port = 8096;
+                }
+                {
+                  name = "portainer";
+                  port = 9000;
+                }
+                {
+                  name = "deluge";
+                  port = 8112;
+                }
+                {
+                  name = "radarr";
+                  port = 7878;
+                }
+                {
+                  name = "slskd";
+                  port = 5030;
+                }
+              ];
             };
           };
-          environmentFile = config.age.secrets.slskd.path;
-        };
 
-        services.deluge = {
-          enable = true;
-          web = {
+          age.secrets.slskd.file = "${inputs.self.outPath}/secrets/slskd";
+
+          services.iperf3.enable = true;
+          services.iperf3.openFirewall = true;
+          services.openssh.enable = true;
+          services.openssh.openFirewall = true;
+          security.sudo.wheelNeedsPassword = false;
+          boot.loader.systemd-boot.enable = true;
+          boot.loader.efi.canTouchEfiVariables = true;
+
+          services.cockpit.enable = true;
+          services.cockpit.openFirewall = true;
+
+          services.slskd = {
             enable = true;
-            port = 8112;
+            domain = null;
+            settings = {
+              shares.directories = [ "/srv/media/Music" ];
+              directories = {
+                incomplete = "/srv/media/Music/download/incomplete";
+                downloads = "/srv/media/Music/download/complete";
+              };
+            };
+            environmentFile = config.age.secrets.slskd.path;
           };
-        };
 
-        services.bazarr.enable = true;
-        services.sonarr.enable = true;
-        services.radarr.enable = true;
-        services.sabnzbd.enable = true;
+          services.deluge = {
+            enable = true;
+            web = {
+              enable = true;
+              port = 8112;
+            };
+          };
 
-        users.groups.media.members = [
-          "slskd"
-          "deluge"
-          "bazarr"
-          "sonarr"
-          "radarr"
-          "sabnzbd"
-          "navidrome"
-          "jellyfin"
-        ];
+          services.bazarr.enable = true;
+          services.sonarr.enable = true;
+          services.radarr.enable = true;
+          services.sabnzbd.enable = true;
 
-        users.users.aiden.extraGroups = [
-          "sadnzbd"
-          "deluge"
-        ];
+          users.groups.media.members = [
+            "slskd"
+            "deluge"
+            "bazarr"
+            "sonarr"
+            "radarr"
+            "sabnzbd"
+            "navidrome"
+            "jellyfin"
+          ];
 
-        networking.firewall.allowedTCPPorts = [ 443 5000 ];
+          users.users.aiden.extraGroups = [
+            "sadnzbd"
+            "deluge"
+          ];
 
-        environment.systemPackages = with pkgs; [
-          get_iplayer
-          wol
-          iperf3
-        ];
-      })
+          networking.firewall.allowedTCPPorts = [
+            443
+            5000
+          ];
+
+          environment.systemPackages = with pkgs; [
+            get_iplayer
+            wol
+            iperf3
+            opencode
+          ];
+        }
+      )
     ];
   };
 }
