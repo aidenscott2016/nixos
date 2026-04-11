ceade685d64ca9235ff010cd842dc54ef858cb81
Aiden <aiden@oldstreetjournal.co.uk>
2026-04-11 15:08:30 +0100

    tmux: Add yank and extrakto plugins
    Switches from xdg.configFile to programs.tmux.extraConfig so
    home-manager can properly initialise plugin run-shell lines.
    Fixes the reload binding path to match the XDG config location.

--- modules/tmux/tmux.nix
+++ modules/tmux/tmux.nix
@@ -3,7 +3,13 @@
   flake.modules.homeManager.tmux =
     { config, pkgs, ... }:
     {
-      programs.tmux.enable = true;
-      xdg.configFile."tmux/tmux.conf".source = ./tmux.conf;
+      programs.tmux = {
+        enable = true;
+        extraConfig = builtins.readFile ./tmux.conf;
+        plugins = with pkgs.tmuxPlugins; [
+          yank
+          extrakto
+        ];
+      };
     };
 }
