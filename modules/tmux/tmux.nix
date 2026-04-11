094058582290ec6b0a0b299476e345c9e089dfac
Aiden <aiden@oldstreetjournal.co.uk>
2026-04-11 15:10:36 +0100

    tmux: Add fzf for extrakto

--- modules/tmux/tmux.nix
+++ modules/tmux/tmux.nix
@@ -3,6 +3,7 @@
   flake.modules.homeManager.tmux =
     { config, pkgs, ... }:
     {
+      home.packages = [ pkgs.fzf ];
       programs.tmux = {
         enable = true;
         extraConfig = builtins.readFile ./tmux.conf;
